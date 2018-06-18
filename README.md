b88878d9e5781f1ac8c532718af04f6892c88f53
Mikkel Ricky <rimi@aarhus.dk>
2018-06-18 20:52:54 +0200

    SALG-397: Added deploy stuff

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # iota
+
+## Installation
+
+```sh
+composer install
+yarn install
+```
+
+Building assets:
+
+```sh
+yarn run encore production
+```
+
+## Deploying
+
+Create and edit `hosts.yaml` (cf. https://deployer.org/docs/hosts).
+
+### Production
+
+```sh
+./vendor/bin/dep deploy production
+```
--- deploy.php
+++ deploy.php
@@ -24,12 +24,25 @@ add('writable_dirs', []);
 
 
 // Hosts
-inventory('hosts.yml');
+inventory('hosts.yaml');
 
 // Tasks
 task('build', function () {
     run('cd {{release_path}} && build');
 });
+
+task('build:assets', function() {
+	// Deploy built assets.
+	run('yarn run encore production');
+})->local();
+
+task('deploy:assets', function() {
+	// Deploy built assets.
+	upload('public/build/', '{{release_path}}/public/build/');
+});
+
+after('deploy:update_code', 'build:assets');
+after('deploy:update_code', 'deploy:assets');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
