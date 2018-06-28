c206a2c62c83e3affec798d485695d01195c3e3c
Mikkel Ricky <rimi@aarhus.dk>
2018-06-28 13:47:31 +0200

    SALG-399: Fixed ui hack

--- assets/js/set_dimming_level.js
+++ assets/js/set_dimming_level.js
@@ -1,7 +1,7 @@
 var $ = require('jquery');
 
 $(document).ready(() => {
-    const $handle = $('#set_dimming_level_value')
+    const $handle = $('#form_value')
     const $label = $('[for="'+$handle.attr('id')+'"]')
     const update = () => {
 		$label.html($handle.val())
